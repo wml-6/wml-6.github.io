520544e3a07c613fb044ac7559349cfdec426a6a
wml-6 <wml19549162860@163.com>
2025-02-12 15:54:28 +0800

    profile

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // 改为你的仓库名，如果部署到根目录则改为 '/'
+  base: "/origin/", // 改为你的仓库名，如果部署到根目录则改为 '/'
 });
